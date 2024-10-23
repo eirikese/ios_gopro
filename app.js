e12747e7c672434f1489e8b7fabf4d259b90835d
eirikese <118887178+eirikese@users.noreply.github.com>
2024-10-23 11:07:44 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('videoPlayer');
+    var videoSrc = 'http://10.5.5.9:8080/live/amba.m3u8';
+
+    // Check if the browser can play HLS natively (Safari on iOS)
+    if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = videoSrc;
+    } else if (Hls.isSupported()) {
+        // Use hls.js to play HLS streams in other browsers
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+    } else {
+        alert('Your browser does not support HLS streaming.');
+    }
+});
